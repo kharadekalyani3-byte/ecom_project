22b56a443d79c40ac2f2fee861096d3daffd629d
kharadekalyani3-byte <kharadekalyani3@gmail.com>
2026-01-09 10:25:52 +0530

    Update MySQL connection details

--- conn.js
+++ conn.js
@@ -1,10 +1,10 @@
 var mysql = require('mysql2');
 var util = require('util');
 var conn = mysql.createConnection({
-    host: 'localhost',
-    user: 'root',
-    password: 'root',
-    database: 'a2z48_ecom'
+    host: 'bvgj3nhk4aui7cnfakby-mysql.services.clever-cloud.com',
+    user: 'uuj1fe9oydcebghm',
+    password: 'JDA0C5DoWViftiopSP2u',
+    database: 'bvgj3nhk4aui7cnfakby'
 });
 var exec = util.promisify(conn.query).bind(conn);
 module.exports = exec;
